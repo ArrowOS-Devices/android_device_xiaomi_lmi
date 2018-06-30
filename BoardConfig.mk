06f53ee26c61cc4c09a368b7678850bd2757b93b
Bruno Martins <bgcngm@gmail.com>
2018-06-30 10:49:31 +0100

    lmi: Import BT config header
     * Straight from CAF (as per LA.UM.6.3.r4-04300-sdm845.0),
       with BTM_DEF_LOCAL_NAME unset so that ro.product.model
       is used instead.
    
    Change-Id: I20a8460c9d936cc3d94f74e469f8babf1c02bda4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,6 +44,9 @@ TARGET_KERNEL_CONFIG := vendor/lmi_user_defconfig
 # Platform
 TARGET_BOARD_PLATFORM := kona
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_lmi
 TARGET_RECOVERY_DEVICE_MODULES := libinit_lmi
