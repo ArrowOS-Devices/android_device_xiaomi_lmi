KONA-DEVS/device_xiaomi_lmi

lmi: light: Do not turn on breathing for unlit hardware states
Change-Id: I093fb2fb81012a473eda232d3c3407d0e0791907
Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

light/Light.cpp
@@ -95,6 +95,11 @@ static void handleNotification(const LightState& state) {
 
     /* Disable breathing or blinking */
     set(NOTIFICATION_LED BREATH, 0);
+    set(NOTIFICATION_LED BRIGHTNESS, 0);
+
+    if (!notificationBrightness) {
+        return;
+    }
 
     switch (state.flashMode) {
         case Flash::HARDWARE:

ArrowOS-Devices/android_device_xiaomi_lmi

light/Light.cpp
@@ -95,6 +95,11 @@ static void handleNotification(const LightState& state) {
 
     /* Disable breathing or blinking */
     set(BLUE_LED BREATH, 0);
+    set(BLUE_LED BRIGHTNESS, 0);
+
+    if (!blueBrightness) {
+        return;
+    }
 
     switch (state.flashMode) {
         case Flash::HARDWARE: