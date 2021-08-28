50baeb5e854f13aafc91e87c9785741674ab70ed
TH779 <i@779.moe>
2021-08-28 13:15:28 +0800

    lmi: Update blobs from V12.5.4.0.RJKCNXM
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Iff63ed563ca61a0aff5bd87c52436ca72602bff1

--- lineage_lmi.mk
+++ lineage_lmi.mk
@@ -25,4 +25,4 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Redmi/lmi/lmi:11/RKQ1.200826.002/V12.5.2.0.RJKCNXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/lmi/lmi:11/RKQ1.200826.002/V12.5.4.0.RJKCNXM:user/release-keys
